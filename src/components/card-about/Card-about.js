5cf153977fae01ec4b57fd28ea8572cd705248b8
G S Patel <girijapatel@directbasing.com>
2020-12-20 20:44:45 +0530

    a tag changeed into Link tag

--- src/components/card-about/Card-about.js
+++ src/components/card-about/Card-about.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 function CardAbout() {
      return (
-          <a href="#" className="card-about">
-               <img src={process.env.PUBLIC_URL+'/img/about-card01.png'} alt="" />
+          <Router>
+               <Link to="/" className="card-about">
+                    <img src={process.env.PUBLIC_URL + '/img/about-card01.png'} alt="" />
 
-               <div className="card-about__content">
-                    <div className="card-about__content-title">Lorem ipsum dolor</div>
+                    <div className="card-about__content">
+                         <div className="card-about__content-title">Lorem ipsum dolor</div>
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
-                    <span className="button button--more">Bekijk al onze prijslijsten</span>
-               </div>
-          </a>
+                         <span className="button button--more">Bekijk al onze prijslijsten</span>
+                    </div>
+               </Link>
+          </Router>
      )
 }
 export default CardAbout;
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,105 +1,108 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 function Footer() {
      return (
-          <footer className="footer">
-               <div className="footer__curve">
-                    <img src={process.env.PUBLIC_URL + '/img/footer-curve.svg'} alt="" />
-               </div>
-
-               <div className="footer__button container">
-                    <a href="#" className="button button--chat">Chat met maxx</a>
-               </div>
-
-               <div className="footer__main container">
-                    <div className="footer__contact">
-                         <div className="footer__contact-title">Trimaxx</div>
-
-                         <address className="footer__contact-address">Huiskensstraat 72, <br />5916 PN Venlo</address>
-
-                         <a className="footer__contact-tel" href="tel:+31 (0)77 206 63 00">+31 (0)77 206 63 00</a><br />
-
-                         <a className="footer__contact-email" href="mailto:info@trimaxx.nl">info@trimaxx.nl</a>
+          <Router>
+               <footer className="footer">
+                    <div className="footer__curve">
+                         <img src={process.env.PUBLIC_URL + '/img/footer-curve.svg'} alt="" />
                     </div>
 
-                    <div className="footer__menu">
-                         <div className="footer__menu-title">Onze diensten</div>
-
-                         <ul className="footer__menu-list">
-                              <li className="footer__menu-item">
-                                   <a href="#" className="footer__menu-link">Vaste telefonie</a>
-                              </li>
-                              <li className="footer__menu-item">
-                                   <a href="#" className="footer__menu-link">Mobiele telefonie</a>
-                              </li>
-                              <li className="footer__menu-item">
-                                   <a href="#" className="footer__menu-link">Internet verbindingen</a>
-                              </li>
-                         </ul>
+                    <div className="footer__button container">
+                         <Link to="/" className="button button--chat">Chat met maxx</Link>
                     </div>
 
-                    <div className="footer__menu">
-                         <div className="footer__menu-title">Meer informatie</div>
+                    <div className="footer__main container">
+                         <div className="footer__contact">
+                              <div className="footer__contact-title">Trimaxx</div>
 
-                         <ul className="footer__menu-list">
-                              <li className="footer__menu-item">
-                                   <a href="#" className="footer__menu-link">Over ons</a>
-                              </li>
-                              <li className="footer__menu-item">
-                                   <a href="#" className="footer__menu-link">Ons team</a>
-                              </li>
-                              <li className="footer__menu-item">
-                                   <a href="#" className="footer__menu-link">Contact</a>
-                              </li>
-                              <li className="footer__menu-item">
-                                   <a href="#" className="footer__menu-link">Offerte</a>
-                              </li>
-                         </ul>
-                    </div>
+                              <address className="footer__contact-address">Huiskensstraat 72, <br />5916 PN Venlo</address>
 
-                    <div className="footer__advice">
-                         <div className="footer__advice-title">Persoonlijk advies nodig?</div>
+                              <a className="footer__contact-tel" href="tel:+31 (0)77 206 63 00">+31 (0)77 206 63 00</a><br />
 
-                         <div className="footer__advice-wrap">
-                              <div className="footer__advice-image">
-                                   <img src={process.env.PUBLIC_URL + '/img/footer.png'} alt="" />
+                              <a className="footer__contact-email" href="mailto:info@trimaxx.nl">info@trimaxx.nl</a>
+                         </div>
+
+                         <div className="footer__menu">
+                              <div className="footer__menu-title">Onze diensten</div>
+
+                              <ul className="footer__menu-list">
+                                   <li className="footer__menu-item">
+                                        <Link top="/" className="footer__menu-link">Vaste telefonie</Link>
+                                   </li>
+                                   <li className="footer__menu-item">
+                                        <Link to="/" className="footer__menu-link">Mobiele telefonie</Link>
+                                   </li>
+                                   <li className="footer__menu-item">
+                                        <Link to="/" className="footer__menu-link">Internet verbindingen</Link>
+                                   </li>
+                              </ul>
+                         </div>
+
+                         <div className="footer__menu">
+                              <div className="footer__menu-title">Meer informatie</div>
+
+                              <ul className="footer__menu-list">
+                                   <li className="footer__menu-item">
+                                        <Link to="/" className="footer__menu-link">Over ons</Link>
+                                   </li>
+                                   <li className="footer__menu-item">
+                                        <Link to="/" className="footer__menu-link">Ons team</Link>
+                                   </li>
+                                   <li className="footer__menu-item">
+                                        <Link to="/" className="footer__menu-link">Contact</Link>
+                                   </li>
+                                   <li className="footer__menu-item">
+                                        <Link to="/" className="footer__menu-link">Offerte</Link>
+                                   </li>
+                              </ul>
+                         </div>
+
+                         <div className="footer__advice">
+                              <div className="footer__advice-title">Persoonlijk advies nodig?</div>
+
+                              <div className="footer__advice-wrap">
+                                   <div className="footer__advice-image">
+                                        <img src={process.env.PUBLIC_URL + '/img/footer.png'} alt="" />
+                                   </div>
+
+                                   <div className="footer__advice-content">
+                                        <p>Wij zijn 24/7 bereikbaar<br /> voor onze klanten via:</p>
+
+                                        <a className="footer__advice-tel" href="tel:+31 77 324 2333">+31 77 324 2333</a>
+                                   </div>
                               </div>
+                         </div>
 
-                              <div className="footer__advice-content">
-                                   <p>Wij zijn 24/7 bereikbaar<br /> voor onze klanten via:</p>
-
-                                   <a className="footer__advice-tel" href="tel:+31 77 324 2333">+31 77 324 2333</a>
-                              </div>
+                         <div className="footer__logo">
+                              <Link to="/">
+                                   <img src={process.env.PUBLIC_URL + '/img/footer-logo.svg'} alt="" />
+                              </Link>
                          </div>
                     </div>
 
-                    <div className="footer__logo">
-                         <a href="#">
-                              <img src={process.env.PUBLIC_URL + '/img/footer-logo.svg'} alt="" />
-                         </a>
+                    <div className="footer__copyright container">
+                         <Link to="/" className="footer__copyright-logo">
+                              <img src={process.env.PUBLIC_URL + '/img/footer-logo-icon.svg'} alt="" />
+                         </Link>
+
+                         <p>© 2020 Trimaxx. All Rights Reserved</p>
+
+                         <ul className="footer__copyright-list">
+                              <li className="footer__copyright-item">
+                                   <Link to="/" className="footer__copyright-link">Algemene voorwaarden</Link>
+                              </li>
+                              <li className="footer__copyright-item">
+                                   <Link to="/" className="footer__copyright-link">Privacy statement</Link>
+                              </li>
+                              <li className="footer__copyright-item">
+                                   <Link to="/" className="footer__copyright-link">Cookies</Link>
+                              </li>
+                         </ul>
                     </div>
-               </div>
-
-               <div className="footer__copyright container">
-                    <a href="#" className="footer__copyright-logo">
-                         <img src={process.env.PUBLIC_URL + '/img/footer-logo-icon.svg'} alt="" />
-                    </a>
-
-                    <p>© 2020 Trimaxx. All Rights Reserved</p>
-
-                    <ul className="footer__copyright-list">
-                         <li className="footer__copyright-item">
-                              <a href="#" className="footer__copyright-link">Algemene voorwaarden</a>
-                         </li>
-                         <li className="footer__copyright-item">
-                              <a href="#" className="footer__copyright-link">Privacy statement</a>
-                         </li>
-                         <li className="footer__copyright-item">
-                              <a href="#" className="footer__copyright-link">Cookies</a>
-                         </li>
-                    </ul>
-               </div>
-          </footer>
+               </footer>
+          </Router>
      )
 }
 export default Footer;
